e06f6c0d40563a18e0399dae36ae921f7b1408ef
Seraphina <211722638+seraph404@users.noreply.github.com>
2025-06-07 12:47:39 -0400

    Create form field validation checks, and generate a unique ID for each item

--- src/controller.js
+++ src/controller.js
@@ -8,6 +8,7 @@ export class Controller {
 
     this.registerViewEvents();
   }
+
   // this makes the following methods available to 'View'
   registerViewEvents() {
     this.view.bindOnAdd(this.onAdd.bind(this));
@@ -16,8 +17,15 @@ export class Controller {
   }
 
   onAdd({ id, title, description, dueDate, priority }) {
-    // send directive to model
-    this.model.addTodo({ id, title, description, dueDate, priority });
+    id = this.generateId();
+    try {
+      id = this.generateId();
+      this.validateInput({ id, title, description, dueDate, priority });
+      this.model.addTodo({ id, title, description, dueDate, priority });
+      this.view.closeModal();
+    } catch (error) {
+      console.error("Validation failed!", error);
+    }
   }
 
   onEdit({ id, title, description, dueDate, priority }) {
@@ -39,5 +47,45 @@ export class Controller {
 
   todoExists(id) {
     return this.model.todoItems.some((todo) => todo.id === id);
+  }
+
+  generateId() {
+    return crypto.randomUUID();
+  }
+
+  validateInput({ title, priority, dueDate }) {
+    //console.log(`Form data is: ${formData}`);
+    console.log("Validating...");
+    return {
+      title: this.validateTitle(title),
+      priority: this.validatePriority(priority),
+      dueDate: this.validateDueDate(dueDate),
+    };
+  }
+
+  validateTitle(title) {
+    // if title doesn't exist or is blank
+    if (!title || title.trim() === "") {
+      throw Error("Title field is required.");
+    }
+    return true;
+  }
+
+  validatePriority(priority) {
+    console.log("validating priority");
+    const allowed = ["Low", "Medium", "High", "Unprioritized"];
+    // throws error
+    if (!allowed.includes(priority)) throw Error("Invalid priority");
+    // if valid
+    return true;
+  }
+
+  validateDueDate(dueDate) {
+    if (!dueDate) return true;
+    const date = new Date(dueDate);
+    if (isNaN(date.getTime())) {
+      throw Error("Invalid due date.");
+    }
+    return true;
   }
 }
