4413abedd0b7d96d15a78799b964667c0fadbea8
Seraphina <211722638+seraph404@users.noreply.github.com>
2025-06-07 14:12:02 -0400

    Change button variable for clarity

--- src/index.js
+++ src/index.js
@@ -6,14 +6,18 @@ import { Controller } from "./controller.js";
 
 /* DOM elements */
 const todoItemsDiv = document.querySelector(".todo-items");
-const addItemBtn = document.querySelector("[data-action='add']");
+const openModalBtn = document.querySelector("[data-action='open-modal']");
+const createTodoBtn = document.querySelector("[data-action='create-todo']");
+const cancelTodoBtn = document.querySelector("[data-action='cancel-todo']");
 
 /* initiate modules */
 const model = new Model();
 window.model = model; // allows me to test in browser console
 const view = new View({
-  addButton: addItemBtn,
+  openModalBtn: openModalBtn,
   todoItems: todoItemsDiv,
+  createTodoBtn: createTodoBtn,
+  cancelTodoBtn: cancelTodoBtn,
 });
 const controller = new Controller(model, view);
 window.controller = controller; // allows me to test in browser console
