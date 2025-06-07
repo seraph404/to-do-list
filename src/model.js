02f1fb8f3007a6caed67be97252733bf0bc9d359
Seraphina <211722638+seraph404@users.noreply.github.com>
2025-06-07 10:22:19 -0400

    Begin building out model for CRUD actions, created dummy content for testing, made a getter method for exposing the todo object

--- src/model.js
+++ src/model.js
@@ -1,3 +1,56 @@
 export class Model {
-  constructor() {}
+  constructor() {
+    // some dummy data for testing purposes
+    this.todos = [
+      {
+        description: undefined,
+        dueDate: "06-07-25",
+        id: "01a76d3c-223d-4320-ab05-a00f359e2148",
+        priority: "High",
+        title: "Go to the library",
+      },
+      {
+        description: undefined,
+        dueDate: "06-12-25",
+        id: "01a76d3c-223d-4320-ab05-a00f359e2149",
+        priority: "Medium",
+        title: "Clean out the fridge",
+      },
+    ];
+  }
+
+  addTodo({ title, description, dueDate, priority }) {
+    const id = crypto.randomUUID();
+    const newTodo = new TodoItem({
+      id: id,
+      title: title,
+      description: description,
+      dueDate: dueDate,
+      priority: priority,
+    });
+    this.todos.push(newTodo);
+    console.log(this.todos);
+  }
+
+  editTodo({ id, title, description, dueDate, priority }) {
+    console.log("Proceed with editing.");
+  }
+
+  deleteTodo({ id, title, description, dueDate, priority }) {
+    console.log("Proceed with deletion.");
+  }
+
+  get todoItems() {
+    return this.todos;
+  }
+}
+
+export class TodoItem {
+  constructor({ id, title, description, dueDate, priority }) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+  }
 }
